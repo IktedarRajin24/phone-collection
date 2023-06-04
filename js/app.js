4a89fd8eb12bf2ea5cde2173cc627ec1466d5134
MD. Iktedar Hasan Rushdi Rajin <RushDizPC@MDs-MacBook-Pro-2.local>
2023-06-04 16:37:32 +0600

    phone detail on going

--- js/app.js
+++ js/app.js
@@ -1,30 +1,39 @@
-const loadPhones = async (searchItem) =>{
+const loadPhones = async (searchItem, dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchItem}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
-const displayPhones = phones =>{
+const displayPhoneDetails = (phone) =>{
+    console.log(phone);
+    const phoneDetails = document.getElementById('phone-details-container');
+    phoneDetails.innerHTML=`
+        <p>Brand: ${phone.brand}</p>
+    `
+}
+
+const displayPhones = (phones, dataLimit) =>{
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.innerText = '';
-    const showAll = document.getElementById('show-all-btn');
-    if(phones.length >10){
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length >10){
+        phones = phones.slice(0,10);
         showAll.classList.remove('hidden');
     }
     else{
         showAll.classList.add('hidden');
     }
     const errorMessage = document.getElementById('err-msg');
-    const phoneArray = phones.slice(0,10);
-    if(phoneArray.length === 0){
+    
+    if(phones.length === 0){
         errorMessage.classList.remove('hidden');
     }else{
         errorMessage.classList.add('hidden');
     }
     
-    phoneArray.forEach(phone => {
-        console.log(phone);
+    phones.forEach(phone => {
+        // console.log(phone);
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add("text-center");
         phoneDiv.classList.add("h-full");
@@ -36,16 +45,37 @@ const displayPhones = phones =>{
         
             <h1 class="lg:text-xl md:text-lg text-md font-bold mb-2">${phone.phone_name}</h1>
             <p class="mb-2"><b>Brand:</b> ${phone.brand}</p>
-            <button class="w-9/12 bg-blue-400 text-white p-2 rounded-lg hover:bg-blue-700">Details</button>
+
+            <!-- Details -->
+            <button id="show-details-btn" class="w-9/12 bg-blue-400 text-white p-2 rounded-lg hover:bg-blue-700" onclick="showDetails('${phone.slug}')" type="button">Details</button>
+
         </div>`
         phoneContainer.appendChild(phoneDiv);
     });
     toggleSpinner(false);
 }
-document.getElementById('search-btn').addEventListener('click', ()=>{
+
+const showDetails =async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const searchField = dataLimit =>{
     const searchItem = document.getElementById('search-bar').value;
     toggleSpinner(true);
-    loadPhones(searchItem);
+    loadPhones(searchItem, dataLimit);
+}
+
+document.getElementById('search-btn').addEventListener('click', ()=>{
+    searchField(10);
+})
+
+document.getElementById('search-bar').addEventListener('keypress', (event)=>{
+    if(event.key === 'Enter'){
+        searchField(10);
+    }
 })
 
 const toggleSpinner = isLoading =>{
@@ -58,3 +88,17 @@ const toggleSpinner = isLoading =>{
     }
 
 }
+
+document.getElementById('show-all-btn').addEventListener('click', ()=>{
+    searchField();
+})
+
+document.getElementById('show-details-btn').addEventListener('click', ()=>{
+    document.getElementById('phone-details').classList.remove('hidden');
+});
+
+document.getElementById('close-details-btn').addEventListener('click', ()=>{
+    document.getElementById('phone-details').classList.add('hidden');
+});
+
+loadPhones('apple')
